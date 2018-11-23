a8de59bf85014ac7a8ed2accb64862ad4c293479
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-23 10:42:56 +0800

    add home-page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/pages/login/Login'
+import Home from '@/pages/home/Home'
 
 // 路由懒加载
 // component: () => import('@/pages/TokenLogin')
@@ -11,8 +12,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Login',
+      name: 'login',
       component: Login
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home
     }
   ]
 })
