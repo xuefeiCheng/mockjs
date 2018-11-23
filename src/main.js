de9a584ae987272fd24bb13f18c3fbc900f2dd90
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-23 10:30:22 +0800

    引入login界面 更改默认路由为/login

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import MockAdapter from 'axios-mock-adapter'
+// import MockAdapter from 'axios-mock-adapter'
 
 Vue.prototype.axios = axios
 Vue.config.productionTip = false
---
+++ src/pages/login/Login.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <el-form
+    class="login-container"
+    ref="AccountForm"
+    :model="account"
+    :rules="loginRules"
+    label-position="left">
+    <h3>Login</h3>
+    <el-form-item prop="username">
+      <el-input
+        v-model="account.username"
+        type="text"
+        placeholder="账号">
+      </el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input v-model="account.password" type="password" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-checkbox v-model="checked" checked>记住密码</el-checkbox>
+    <el-form-item>
+      <el-button type="primary">login</el-button>
+      <el-button type="primary">reset</el-button>
+    </el-form-item>
+  </el-form>
+
+</template>
+
+<script>
+// import {requseLogin} from "../axios/api"
+
+export default {
+  name: 'login',
+  data () {
+    return {
+      account: {
+        username: '',
+        password: ''
+      },
+      loginRules: {
+        username: [{required: true, message: '请输入账号', trigger: 'blur'}],
+        password: [{required: true, message: '请输入密码', trigger: 'blur'}]
+      },
+      checked: true,
+      logining: false
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+body{
+  background: #DFE9FB;
+}
+.login-container {
+  width:350px;
+  margin-left:35%;
+  border: 1px solid #d3d3d3;
+  box-sizing: border-box;
+  padding: 10px 30px;
+  border-radius: 5px;
+}
+.el-button {
+  width:100%;
+  box-sizing: border-box;
+  margin: 10px 0;
+}
+</style>
