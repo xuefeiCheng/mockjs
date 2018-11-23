d62af359289ee795b08fc789143cebe1deaf7a34
xuefeicheng <xuefeicheng_ytu@163.com>
2018-11-23 10:34:27 +0800

    router lazy load

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/pages/login/Login'
 
+// 路由懒加载
+// component: () => import('@/pages/TokenLogin')
+const Login = resolve => require(['@/pages/login/Login'], resolve)
 Vue.use(Router)
 
 export default new Router({
